8661648f5d39c2631388bcd4255057a0fb7c5bc7
samprit9122000 <samprit.gullu@gmail.com>
2023-06-17 23:59:30 +0530

    Registration component form validation

--- frontend/src/components/forms/Regform.js
+++ frontend/src/components/forms/Regform.js
@@ -1,16 +1,59 @@
 import React from 'react'
 import './form.css'
+import {useFormik} from 'formik'
+import signupschema from '../../schemas/formschema'
+
+const initialvalue={
+    username:"",
+    email:"",
+    password:"",
+    confirm_pass:""
+
+}
 
 function Regform() {
+
+    const {values,touched,errors,handleBlur,handleChange,handleSubmit}=useFormik({
+        initialValues:initialvalue,
+        validationSchema:signupschema,
+        onSubmit: (val)=>{
+            console.log(val)
+        }
+    })
+
+    console.log(errors)
+
   return (
-    <div className='reg'>
+    <div className='reg' onSubmit={handleSubmit}>
       <form>
         <h2>User Registration</h2>
-      <input type='text' name='username' placeholder='Username'/>
-      <input type='email' name='email' placeholder='Email'/>
-      <input type='password' name='password' placeholder='Password'/>
-      <input type='password' name='confirm_pass' placeholder='Confirm password'/>
-      <button>Submit</button>
+      <input type='text' name='username' placeholder='Username'
+      value={values.username} onChange={handleChange} onBlur={handleBlur}/>
+      {
+        errors.username && touched.username?<p>{errors.username}</p>:null
+      }
+
+      <input type='email' name='email' placeholder='Email'
+      value={values.email} onChange={handleChange} onBlur={handleBlur}/>
+       {
+        errors.email && touched.email?<p>{errors.email}</p>:null
+      }
+
+      <input type='password' name='password' placeholder='Password'
+      value={values.password} onChange={handleChange} onBlur={handleBlur}/>
+       {
+        errors.password && touched.password?<p>{errors.password}</p>:null
+      }
+
+      <input type='password' name='confirm_pass' placeholder='Confirm password'
+      value={values.confirm_pass} onChange={handleChange} onBlur={handleBlur}/>
+       {
+        errors.confirm_pass && touched.confirm_pass?<p>{errors.confirm_pass}</p>:null
+      }
+
+
+      <input type='submit' id='button' value='Register'/>
+
       </form>  
 
     </div>
---
+++ frontend/src/schemas/formschema.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup'
+
+const signupschema=Yup.object({
+    username:Yup.string().min(2).max(25).required("Please enter your name"),
+    email:Yup.string().email().required("Enter your email"),
+    password:Yup.string().min(6).max(10).required("Enter your password"),
+    confirm_pass:Yup.string().required().oneOf([Yup.ref('password'),null],'password does not matched'),
+})
+
+export default signupschema;
